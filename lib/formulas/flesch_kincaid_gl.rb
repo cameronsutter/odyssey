d20176634aceaf5b9d07ccaad5d29bbebfbf0112
Michael Lahnert <michael.lahnert@ipvs.uni-stuttgart.de>
2018-12-19 17:14:01 +0100

    By sentence for Flesch Kincaid readability grade

--- lib/formulas/flesch_kincaid_gl.rb
+++ lib/formulas/flesch_kincaid_gl.rb
@@ -4,6 +4,16 @@ class FleschKincaidGl < Formula
     calc_score(stats['average_words_per_sentence'], stats['average_syllables_per_word'])
   end
 
+  def score_by_sentence(text, stats_split)
+    res = []
+    for i in 0..text['sentences'].length-1
+      res.push(calc_score(stats_split['word_count'][i],
+                          stats_split['average_syllables_per_word'][i]))
+    end
+    res
+  end
+
+
   def calc_score(avg_words, avg_syllables)
     (((0.39 * avg_words) + (11.8 * avg_syllables)) - 15.59).round(1)
   end
