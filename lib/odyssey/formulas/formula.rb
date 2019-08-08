03b15e18416ce26437e4b8edf3d54b1db1d4f54b
Mark Delk <jethrodaniel@gmail.com>
2019-08-08 02:12:38 -0400

    reduce merge diff

--- lib/odyssey/formulas/formula.rb
+++ lib/odyssey/formulas/formula.rb
@@ -1,35 +1,32 @@
-module Odyssey
-  class Formula
+class Odyssey::Formula
+  # text will be a Hash like so:
+  # data = {
+  #   'raw' => String,
+  #   'words' => Array,
+  #   'sentences' => Array,
+  #   'syllables' => Array
+  # }
+  #
+  # stats will be a Hash like so:
+  # {
+  #   'string_length' => Fixnum,
+  #   'letter_count' => Fixnum,
+  #   'syllable_count' => Fixnum,
+  #   'word_count' => Fixnum,
+  #   'sentence_count' => Fixnum,
+  #   'average_words_per_sentence' => Float,
+  #   'average_syllables_per_word' => Float
+  # }
 
-    # text will be a Hash like so:
-    # data = {
-    #   'raw' => String,
-    #   'words' => Array,
-    #   'sentences' => Array,
-    #   'syllables' => Array
-    # }
-    #
-    # stats will be a Hash like so:
-    # {
-    #   'string_length' => Fixnum,
-    #   'letter_count' => Fixnum,
-    #   'syllable_count' => Fixnum,
-    #   'word_count' => Fixnum,
-    #   'sentence_count' => Fixnum,
-    #   'average_words_per_sentence' => Float,
-    #   'average_syllables_per_word' => Float
-    # }
+  def score(text, stats)
+    0
+  end
 
-    def score(text, stats)
-      0
-    end
+  def score_by_sentence(text, stats_split)
+    0
+  end
 
-    def score_by_sentence(text, stats_split)
-      0
-    end
-
-    def name
-      'Generic'
-    end
+  def name
+    'Generic'
   end
 end
